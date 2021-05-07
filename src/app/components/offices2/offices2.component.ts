d824d27b7093c1d0277712869e4a829ab6a93419
Oleg Sokolovsky <Martiniks.so@gmail.com>
2021-05-07 12:53:26 +0300

    mat table whis trick

--- src/app/components/offices2/offices2.component.ts
+++ src/app/components/offices2/offices2.component.ts
@@ -11,14 +11,7 @@ export interface PeriodicElement {
   zamdir?: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { "id": "400", "name": "Филиал Белгосстраха по Гродненской области", "adres": "230012, г. Гродно, ул. Доватора, 2/2", "director": "", "zamdir": "", "glbuh": "" },
-  { "id": "401", "name": " Представительство по г.Гродно", "adres": "230012, г. Гродно, ул. Доватора, 2/2", "director": "", "zamdir": "", "glbuh": "" },
-  { "id": "403", "name": " Представительство по Волковыскому району", "adres": "231900, г. Волковыск, ул. Медведева, 3", "director": "", "zamdir": "", "glbuh": "" },
-  { "id": "404", "name": " Представительство по Вороновскому району", "adres": "231391, г.п. Вороново, ул. Калинина, 4/а", "director": "", "zamdir": "", "glbuh": "" },
-  { "id": "405", "name": " Представительство по Гродненскому району", "adres": "230025, г. Гродно, пр. Космонавтов, 37а", "director": "", "zamdir": "", "glbuh": "" },
-  { "id": "406", "name": " Представительство по Дятловскому району", "adres": "231471, г. Дятлово, ул. Первомайская, 1", "director": "", "zamdir": "", "glbuh": "" },
-];
+const ELEMENT_DATA: PeriodicElement[] = [];
 
 @Component({
   selector: 'app-offices2',
